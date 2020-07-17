73f6335661dd4f1e6ad2de0688b5a95e6349a370
Fabiola Kretzer <fabiolakretzer@gmail.com>
2020-07-17 15:03:10 -0300

    construct query database

---
+++ src/repositories/seach-products-database.ts
@@ -0,0 +1,8 @@
+import knex from '../database/connection'
+
+const seachProducts = async (page: number, productsPerPage: number) => {
+    const res = knex('products').orderBy('productId').limit(productsPerPage).offset(page * productsPerPage)  
+    return res
+}
+
+export { seachProducts }
--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,21 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
+import mainProducer from './server/index-producer'
 
 const routes = express.Router()
+
+routes.get('/products', async function(request: Request, response: Response) {
+  const { page, productsPerPage } = request.body
+
+  if(!page || !productsPerPage) {
+    console.log('Atributos inválidos!!!')
+    return
+  }
+
+  console.log(page)
+  console.log(productsPerPage)
+
+  const products = await mainProducer(page, productsPerPage)
+  response.json(products)
+})
 
 export default routes
