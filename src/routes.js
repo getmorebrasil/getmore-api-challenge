2cd3cd9e9fa5f4e78baa0edb727227e9b5711ca0
Geyslan G. Bem <geyslan@gmail.com>
2020-08-15 01:42:51 -0300

    add paging
    egg. `/products/?n=15&p=3`
    
    Signed-off-by: Geyslan G. Bem <geyslan@gmail.com>

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,18 @@ const connection = require("./database/connection");
 const routes = express.Router();
 
 routes.get("/products", async (req, res) => {
-  const prods = await connection("products").select("*");
+  var { n = 10, p = 1 } = req.query;
+  if (n < 1) {
+    n = 10;
+  }
+  if (p < 1) {
+    p = 1;
+  }
+
+  const prods = await connection("products")
+    .limit(n)
+    .offset((p - 1) * n)
+    .select("*");
 
   return res.json(prods);
 });
