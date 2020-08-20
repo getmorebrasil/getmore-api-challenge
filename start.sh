1ae56b4909d2d1902458b3fc124d630ac7ec0918
Pedro Nobrega <pedro.dias.nobrega@gmail.com>
2020-08-20 00:51:08 -0300

    Add clone submodeles within start script

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
+git submodule init
+git submodule update
 docker exec -d node sh -c "yarn typeorm migration:run && yarn ts-node src/database/populate.ts"
 sleep 5
 docker exec node sh -c "yarn dev:server"
