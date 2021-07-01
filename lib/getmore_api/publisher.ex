1a607e8516392760a9f050cada0713122a4654b4
Igor Fernandes <igorkuivjogi@hotmail.com>
2021-06-30 22:47:14 -0300

    added rabbitmq configs

---
+++ lib/getmore_api/publisher.ex
@@ -0,0 +1,6 @@
+# defmodule GetmoreApi.Publisher do
+#   alias AMQP
+
+#   {:ok, connection} = AMQP.Connection.open()
+#   {:ok, channel} = AMQP.Channel.open(connection)
+# end
--- lib/getmore_api/repo.ex
+++ lib/getmore_api/repo.ex
@@ -3,15 +3,15 @@ defmodule GetmoreApi.Repo do
     otp_app: :getmore_api,
     adapter: Ecto.Adapters.Postgres
 
-  # def init(_, config) do
-  #   config =
-  #     config
-  #     |> Keyword.put(:username, System.get_env("PGUSER"))
-  #     |> Keyword.put(:password, System.get_env("PGPASSWORD"))
-  #     |> Keyword.put(:database, System.get_env("PGDATABASE"))
-  #     |> Keyword.put(:hostname, System.get_env("PGHOST"))
-  #     |> Keyword.put(:port, System.get_env("PGPORT") |> String.to_integer())
+  def init(_, config) do
+    config =
+      config
+      |> Keyword.put(:username, System.get_env("PGUSER", "postgres"))
+      |> Keyword.put(:password, System.get_env("PGPASSWORD", "postgres"))
+      |> Keyword.put(:database, System.get_env("PGDATABASE", "getmore_api_dev"))
+      |> Keyword.put(:hostname, System.get_env("PGHOST", "localhost"))
+      |> Keyword.put(:port, System.get_env("PGPORT", "5432") |> String.to_integer())
 
-  #   {:ok, config}
-  # end
+    {:ok, config}
+  end
 end
