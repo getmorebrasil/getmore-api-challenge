ddd9c3235069ef848381c5c02830d1f528011770
Eduardo Baptista <eduardo.baptista@vulcanet.com.br>
2021-07-02 15:57:27 -0300

    PRODUCTS: Use jason

--- products/mix.exs
+++ products/mix.exs
@@ -24,7 +24,8 @@ defmodule Products.MixProject do
     [
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
       {:ecto_sql, "~> 3.0"},
-      {:postgrex, ">= 0.0.0"}
+      {:postgrex, ">= 0.0.0"},
+      {:jason, "~> 1.2"}
     ]
   end
 end
