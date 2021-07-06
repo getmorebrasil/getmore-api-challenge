6c94d00ee3d6780663665f50443db72293111c59
Eduardo <eduardofbaptista@gmail.com>
2021-07-06 17:56:53 -0300

    API: Use credo

--- api/mix.exs
+++ api/mix.exs
@@ -39,7 +39,8 @@ defmodule Api.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false}
     ]
   end
 
