57e7ce23ee4c5906b9be51947ed360b7576c1bf8
Eduardo <eduardofbaptista@gmail.com>
2021-07-06 17:56:13 -0300

    Create API project

---
+++ api/config/config.exs
@@ -0,0 +1,28 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
+use Mix.Config
+
+# Configures the endpoint
+config :api, ApiWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "gCsRBZGSQV4O77uCsztvV2uf1dBOHGXm0NLNuR90EREof4nk2uFTWXujBE0HNjuY",
+  render_errors: [view: ApiWeb.ErrorView, accepts: ~w(json), layout: false],
+  pubsub_server: Api.PubSub,
+  live_view: [signing_salt: "nZKBguWt"]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
