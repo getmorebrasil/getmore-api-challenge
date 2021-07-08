39bac36b0f825c095b1fcad5e571efd4c85c7036
Eduardo <eduardofbaptista@gmail.com>
2021-07-08 19:11:16 -0300

    PRODUCTS: Add products list method

--- products/lib/products.ex
+++ products/lib/products.ex
@@ -1,18 +1,33 @@
 defmodule Products do
   @moduledoc """
-  Documentation for `Products`.
+  Product context.
   """
+  import Ecto.Query
 
+  alias Products.{Product, Repo}
+
+  @typedoc """
+  List params definition
+  """
+  @type list_params :: %{
+          optional(:page) => integer(),
+          optional(:page_size) => integer()
+        }
+
+  @spec list() :: list(Product.t())
+  @spec list(list_params()) :: list(Product.t())
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Products.hello()
-      :world
-
+  List products by page
   """
-  def hello do
-    :world
+  def list(params \\ %{}) do
+    page = Map.get(params, "page", 1)
+    page_size = Map.get(params, "page_size", 20)
+
+    query =
+      from p in Product,
+        limit: ^page_size,
+        offset: ^((page - 1) * page_size)
+
+    Repo.all(query)
   end
 end
