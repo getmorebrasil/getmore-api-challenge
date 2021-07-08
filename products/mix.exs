62baec653f88f6d8da225ec27570da669585879e
Eduardo <eduardofbaptista@gmail.com>
2021-07-08 19:12:09 -0300

    PRODUCTS: Fix amqp log error

--- products/mix.exs
+++ products/mix.exs
@@ -16,7 +16,7 @@ defmodule Products.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
+      extra_applications: [:lager, :logger],
       mod: {Products.Application, []}
     ]
   end
