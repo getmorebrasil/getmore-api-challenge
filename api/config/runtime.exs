8412d14ce6d6a9b94d8a1ea44e7f87fc6bcaec96
Eduardo <eduardofbaptista@gmail.com>
2021-07-09 20:59:47 -0300

    API: Update config to release

--- api/config/runtime.exs
+++ api/config/runtime.exs
@@ -2,7 +2,7 @@
 # from environment variables. You can also hardcode secrets,
 # although such is generally not recommended and you have to
 # remember to add this file to your .gitignore.
-use Mix.Config
+import Config
 
 secret_key_base =
   System.get_env("SECRET_KEY_BASE") ||
@@ -16,14 +16,7 @@ config :api, ApiWeb.Endpoint,
     port: String.to_integer(System.get_env("PORT") || "4000"),
     transport_options: [socket_opts: [:inet6]]
   ],
-  secret_key_base: secret_key_base
+  secret_key_base: secret_key_base,
+  server: true
 
-# ## Using releases (Elixir v1.9+)
-#
-# If you are doing OTP releases, you need to instruct Phoenix
-# to start each relevant endpoint:
-#
-#     config :api, ApiWeb.Endpoint, server: true
-#
-# Then you can assemble a release by calling `mix release`.
-# See `mix help release` for more information.
+config :api, Api.Products.Get, rabbitmq_uri: System.get_env("RABBITMQ_URI")
