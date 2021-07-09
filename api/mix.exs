c0070d698cf6fda48924682c851efd724bab59a1
Eduardo <eduardofbaptista@gmail.com>
2021-07-09 16:10:16 -0300

    API: Fix amqp log error

--- api/mix.exs
+++ api/mix.exs
@@ -20,7 +20,7 @@ defmodule Api.MixProject do
   def application do
     [
       mod: {Api.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:lager, :logger, :runtime_tools]
     ]
   end
 
