fe3542b02712d5460a00ffa37cde6c9720f9931b
Eduardo <eduardofbaptista@gmail.com>
2021-07-09 16:23:02 -0300

    API: Update error view to handle Error struct

--- api/lib/api_web/views/error_view.ex
+++ api/lib/api_web/views/error_view.ex
@@ -1,16 +1,27 @@
 defmodule ApiWeb.ErrorView do
   use ApiWeb, :view
 
-  # If you want to customize a particular status code
-  # for a certain format, you may uncomment below.
-  # def render("500.json", _assigns) do
-  #   %{errors: %{detail: "Internal Server Error"}}
-  # end
+  import Ecto.Changeset, only: [traverse_errors: 2]
 
-  # By default, Phoenix returns the status message from
-  # the template name. For example, "404.json" becomes
-  # "Not Found".
+  alias Ecto.Changeset
+
   def template_not_found(template, _assigns) do
     %{errors: %{detail: Phoenix.Controller.status_message_from_template(template)}}
+  end
+
+  def render("error.json", %{content: %Changeset{} = changeset}) do
+    %{errors: translate_errors(changeset)}
+  end
+
+  def render("error.json", %{content: content}) when is_binary(content) do
+    %{error: content}
+  end
+
+  defp translate_errors(changeset) do
+    traverse_errors(changeset, fn {msg, opts} ->
+      Enum.reduce(opts, msg, fn {key, value}, acc ->
+        String.replace(acc, "%{#{key}}", to_string(value))
+      end)
+    end)
   end
 end
