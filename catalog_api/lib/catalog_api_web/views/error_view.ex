f6f8db61421fedef2eea4d15fe93ab2fc029e01c
Rayan Castro <rayanrcastro@gmail.com>
2021-07-12 17:17:01 -0300

    chore: add fallback controller

--- catalog_api/lib/catalog_api_web/views/error_view.ex
+++ catalog_api/lib/catalog_api_web/views/error_view.ex
@@ -13,4 +13,20 @@ defmodule CatalogApiWeb.ErrorView do
   def template_not_found(template, _assigns) do
     %{errors: %{detail: Phoenix.Controller.status_message_from_template(template)}}
   end
+
+  def render("500.json", %{error: error}) do
+    %{
+      error: %{
+        detail: error
+      }
+    }
+  end
+
+  def render("400.json", %{error: error}) do
+    %{
+      errors: %{
+        detail: error.detail
+      }
+    }
+  end
 end
